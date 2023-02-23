4e09e2ffc640a95ae8cf8fcac73013ee222a1405
terabytegta <114786257+terabytegta@users.noreply.github.com>
2023-02-23 19:36:23 -0300

    Create app.js

---
+++ react/atividade2.1/src/app.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Eduardo Araujo!!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
